96a6daf2a7dbfdec50438050cd64a19e7a919ab9
Ashvini Maurya <ashvini@gloify.com>
2019-07-13 09:42:24 +0530

    showing list of devices

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,40 +1,28 @@
 import React, { Component } from "react";
-// import auth from "./auth";
+import auth from "../../auth";
 import axios from "axios";
-
-// export const AppLayout = props => {
-//   return (
-//     <div>
-//       <h1>app layout</h1>
-//       <button
-//         onClick={() => {
-//           auth.logout(() => {
-//             props.history.push("/");
-//           });
-//         }}
-//       >
-//         LogOut
-//       </button>
-//     </div>
-//   );
-// };
+import "./AppLayout.css";
 
 class AppLayout extends Component {
-  
   state = {
-    token: localStorage.getItem("token")
-  }
+    token: localStorage.getItem("token"),
+    devices: []
+  };
 
   componentDidMount() {
-    axios.defaults.headers.common = {'Authorization': `Bearer ${this.state.token}`}
+    axios.defaults.headers.common = {
+      Authorization: `Bearer ${this.state.token}`
+    };
     axios({
       method: "get",
       url:
-        "https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com/latest/devices",
-        
+        "https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com/latest/devices"
     })
       .then(res => {
         console.log("device res: ", res);
+        this.setState({
+          devices: res.data.result
+        });
       })
       .catch(err => {
         console.log(err);
@@ -42,7 +30,37 @@ class AppLayout extends Component {
   }
 
   render() {
-    return <h1>hi</h1>;
+    return (
+      <div className="container-fluid">
+        <div className="pt-4 px-2">
+          <div className="row">
+            <div className="col-md-10">
+              <h1>here goes the maps</h1>
+            </div>
+
+            <div className="col-md-2">
+              <div className="listOfDevices">
+                <h4>Devices</h4>
+                {this.state.devices.map(device => (
+                  <p key={device.device}>{device.device}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <button
+            onClick={() => {
+              auth.logout(() => {
+                localStorage.clear();
+                this.props.history.push("/");
+              });
+            }}
+          >
+            LogOut
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
